230886bd81240bc6502c0be1905c3abe49b69749
Mahmood Barzegar <m.barzegar@mediana.ir>
2023-07-12 13:37:19 +0330

    :up: :(Demo) Added demo data

---
+++ src/demo/WikiData.js
@@ -0,0 +1,269 @@
+export const wikiList = [
+  {
+    id: "53",
+    title: "PHP Standard Recommendation",
+    link: "https://en.wikipedia.org/wiki/PHP_Standard_Recommendation",
+  },
+  {
+    id: "52",
+    title: "How to Install Swagger Editor and UI Locally ?",
+    link: "https://www.datasciencelearner.com/how-to-install-swagger-editor-and-ui-locally/",
+  },
+  {
+    id: "51",
+    title: "How to Install Jupyter notebook on Ubuntu 22.04",
+    link: "https://linuxhint.com/install-jupyter-notebook-ubuntu-22-04/",
+  },
+  {
+    id: "50",
+    title: "How To Install the Anaconda Python Distribution on Ubuntu 22.04",
+    link: "https://www.digitalocean.com/community/tutorials/how-to-install-the-anaconda-python-distribution-on-ubuntu-22-04",
+  },
+  {
+    id: "49",
+    title: "What are the Basic AI Concepts?",
+    link: "https://www.techslang.com/what-are-the-basic-concepts-in-ai/#:~:text=What%20is%20AI%2C%20and%20How,in%20systems%20to%20varying%20levels.",
+  },
+  {
+    id: "48",
+    title: "Calculating InnoDB Buffer Pool Size for your MySQL Server",
+    link: "https://scalegrid.io/blog/calculating-innodb-buffer-pool-size-for-your-mysql-server/",
+  },
+  {
+    id: "47",
+    title: "Delete all Duplicate Rows except for One in MySQL? [duplicate]",
+    link: "https://stackoverflow.com/questions/4685173/delete-all-duplicate-rows-except-for-one-in-mysql",
+  },
+  {
+    id: "46",
+    title:
+      'Android : Intel x86 Emulator Accelerator HAXM installer - "revision: 7.5.6" update failed',
+    link: "https://stackoverflow.com/questions/60544578/android-intel-x86-emulator-accelerator-haxm-installer-revision-7-5-6-upda",
+  },
+  {
+    id: "45",
+    title: "Understanding relative and absolute imports in Next.js",
+    link: "https://blog.logrocket.com/understanding-relative-absolute-imports-next-js/",
+  },
+  {
+    id: "44",
+    title: "PWA Manifest Generator",
+    link: "https://www.simicart.com/manifest-generator.html/",
+  },
+  {
+    id: "43",
+    title: "Failed to compile error during creating PWA with next.js",
+    link: "https://stackoverflow.com/questions/73580892/failed-to-compile-error-during-creating-pwa-with-next-js",
+  },
+  {
+    id: "42",
+    title: "Dockerizing a Next.js Static HTML app",
+    link: "https://www.oddicles.net/blog/dockerizing-a-next-js-static-html-app/",
+  },
+  {
+    id: "41",
+    title:
+      "How To Install and Set Up Laravel with Docker Compose on Ubuntu 22.04",
+    link: "https://www.digitalocean.com/community/tutorials/how-to-install-and-set-up-laravel-with-docker-compose-on-ubuntu-22-04",
+  },
+  {
+    id: "40",
+    title:
+      "What are the proper permissions for an upload folder with PHP/Apache?",
+    link: "https://stackoverflow.com/questions/10990/what-are-the-proper-permissions-for-an-upload-folder-with-php-apache",
+  },
+  {
+    id: "39",
+    title: "Implementing JWT authentication in Laravel 9",
+    link: "https://blog.logrocket.com/implementing-jwt-authentication-laravel-9/",
+  },
+  {
+    id: "38",
+    title: "How to Check Out a Remote Git Branch?",
+    link: "https://www.studytonight.com/git-guide/how-to-check-out-a-remote-git-branch#:~:text=We%20will%20use%20the%20Git,and%20even%20add%20new%20ones.",
+  },
+  {
+    id: "37",
+    title: "What Is Canary Deployment?",
+    link: "https://semaphoreci.com/blog/what-is-canary-deployment#:~:text=In%20software%20engineering%2C%20canary%20deployment,the%20rest%20of%20the%20users.",
+  },
+  {
+    id: "36",
+    title: "What Is Blue-Green Deployment?",
+    link: "https://semaphoreci.com/blog/blue-green-deployment",
+  },
+  {
+    id: "35",
+    title: "How to fix MySQL not starting in XAMPP",
+    link: "https://write.corbpie.com/how-to-fix-mysql-not-starting-in-xampp/",
+  },
+  {
+    id: "34",
+    title: "Error: No configuration files have been found in your configDir",
+    link: "https://github.com/storybookjs/storybook/issues/17751",
+  },
+  {
+    id: "33",
+    title: " Cumulative Layout Shift",
+    link: "https://mizfa.com/blog/gtmetrix-new-cumulative-layout-shift/",
+  },
+  {
+    id: "32",
+    title: "Avoid non-composited animations",
+    link: "https://mizfa.com/blog/gtmetrix-new-avoid-non-composited-animations/",
+  },
+  {
+    id: "31",
+    title: "What does the exclamation mark mean before a function call?",
+    link: "https://stackoverflow.com/questions/63253015/what-does-the-exclamation-mark-mean-before-a-function-call",
+  },
+  {
+    id: "30",
+    title: "PHP-FPM does not automatically start after reboot",
+    link: "https://serverfault.com/questions/459728/php-fpm-does-not-automatically-start-after-reboot",
+  },
+  {
+    id: "29",
+    title: "How To Undo Last Git Commit",
+    link: "https://devconnected.com/how-to-undo-last-git-commit/#:~:text=The%20easiest%20way%20to%20undo,changes%20done%20to%20your%20files.",
+  },
+  {
+    id: "28",
+    title: "Flutter doctor error - Android sdkmanager tool not found. Windows",
+    link: "https://stackoverflow.com/questions/60475481/flutter-doctor-error-android-sdkmanager-tool-not-found-windows",
+  },
+  {
+    id: "27",
+    title:
+      "How to merge only specific commits from a pull request with git cherry-pick",
+    link: "https://mattstauffer.com/blog/how-to-merge-only-specific-commits-from-a-pull-request/",
+  },
+  {
+    id: "26",
+    title:
+      "How to completely replace git branch code with another branch's code",
+    link: "https://www.nickang.com/2017-09-30-replace-git-branch-code/",
+  },
+  {
+    id: "25",
+    title: "React Test-driven Development: From User Stories to Production",
+    link: "https://www.toptal.com/react/tdd-react-user-stories-to-development",
+  },
+  {
+    id: "24",
+    title: "Setting ESLint on a React Typescript project (2022)",
+    link: "https://andrebnassis.medium.com/setting-eslint-on-a-react-typescript-project-2021-1190a43ffba",
+  },
+  {
+    id: "23",
+    title: "How to Select the First/Least/Max Row per Group in SQL",
+    link: "https://www.xaprb.com/blog/2006/12/07/how-to-select-the-firstleastmax-row-per-group-in-sql/",
+  },
+  {
+    id: "22",
+    title: "vue-cli-service: Permission denied while running npm run serve",
+    link: "https://stackoverflow.com/questions/62453948/vue-cli-service-permission-denied-while-running-npm-run-serve",
+  },
+  {
+    id: "21",
+    title: "Strict Types in PHP",
+    link: "https://chemaclass.es/blog/strict-types/",
+  },
+  {
+    id: "20",
+    title: "Remote Development with VS Code on Mac in 5 simple steps",
+    link: "https://medium.com/macoclock/remote-development-with-vscode-on-mac-in-simple-5-steps-6ae100938d67",
+  },
+  {
+    id: "19",
+    title: "Webchain Whitepaper",
+    link: "https://www.allcryptowhitepapers.com/webchain-whitepaper/",
+  },
+  {
+    id: "18",
+    title: "Webchain - A Blockchain for DApps, Secured via Websites",
+    link: "https://medium.com/@GemsThrones/webchain-a-blockchain-for-dapps-secured-via-websites-7862167d589e",
+  },
+  {
+    id: "17",
+    title:
+      "A visual guide to the React Native TextInput keyboardType prop values",
+    link: "https://lefkowitz.me/visual-guide-to-react-native-textinput-keyboardtype-options/",
+  },
+  {
+    id: "16",
+    title: "React-native swipeable gesture not working on android?",
+    link: "https://stackoverflow.com/questions/70545275/react-native-swipeable-gesture-not-working-on-android",
+  },
+  {
+    id: "15",
+    title: "How do I implement interfaces in python?",
+    link: "https://stackoverflow.com/questions/2124190/how-do-i-implement-interfaces-in-python",
+  },
+  {
+    id: "14",
+    title:
+      "Make owner of newly create files AND folders www-data instead of superuser/admin",
+    link: "https://askubuntu.com/questions/631303/make-owner-of-newly-create-files-and-folders-www-data-instead-of-superuser-admin",
+  },
+  {
+    id: "13",
+    title: "How to connect to remote Redis server?",
+    link: "https://stackoverflow.com/questions/40678865/how-to-connect-to-remote-redis-server",
+  },
+  {
+    id: "12",
+    title: "SOLID: The First 5 Principles of Object Oriented Design",
+    link: "https://www.digitalocean.com/community/conceptual_articles/s-o-l-i-d-the-first-five-principles-of-object-oriented-design",
+  },
+  {
+    id: "11",
+    title: "What is high level modules and low level modules.?",
+    link: "https://stackoverflow.com/questions/16558256/what-is-high-level-modules-and-low-level-modules",
+  },
+  {
+    id: "10",
+    title:
+      "Reset local repository branch to be just like remote repository HEAD",
+    link: "https://stackoverflow.com/questions/1628088/reset-local-repository-branch-to-be-just-like-remote-repository-head",
+  },
+  {
+    id: "7",
+    title: "How to fix BrowserRouter for React Apps on Apache",
+    link: "https://www.andreasreiterer.at/fix-browserrouter-on-apache/",
+  },
+  {
+    id: "6",
+    title: "Finding duplicate values in a SQL table",
+    link: "https://stackoverflow.com/questions/2594829/finding-duplicate-values-in-a-sql-table",
+  },
+  {
+    id: "5",
+    title: "Getting Started with Beanstalkd",
+    link: "https://lornajane.net/posts/2014/getting-started-with-beanstalkd",
+  },
+  {
+    id: "4",
+    title: "Supervisor socket error issue",
+    link: "https://stackoverflow.com/questions/18859063/supervisor-socket-error-issue",
+  },
+  {
+    id: "3",
+    title: "Delete all Duplicate Rows except for One in MySQL",
+    link: "https://stackoverflow.com/questions/4685173/delete-all-duplicate-rows-except-for-one-in-mysql",
+  },
+  {
+    id: "2",
+    title: "Handling Slow Queries in MongoDB ",
+    link: "https://rockset.com/blog/handling-slow-queries-in-mongodb-part-1-investigation",
+  },
+  {
+    id: "1",
+    title: "The server requested authentication method unknown to the client",
+    link: "https://stackoverflow.com/questions/52364415/php-with-mysql-8-0-error-the-server-requested-authentication-method-unknown-to",
+  },
+];
+
+export function getWikiList() {
+  return wikiList.filter((g) => g);
+}
